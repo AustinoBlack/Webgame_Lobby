5a21960c19209c7a0fc0bf1aa8ea1ae6bec8eb2c
AustinoBlack <baustinblack98@gmail.com>
2023-05-10 13:56:26 -0400

    fixed a variable naming error in Lobby.html. added 'ON DELETE CASCADE' to relations hosts and joins in order to remove data appropiately when leave_lobby is called

--- db_schema/primary.sql
+++ db_schema/primary.sql
@@ -4,24 +4,24 @@ DROP TABLE IF EXISTS lobby;
 DROP TABLE IF EXISTS player;
 
 CREATE TABLE lobby (
-    roomcode VARCHAR(4) PRIMARY KEY,
-    roomsize INTEGER
+    roomcode VARCHAR(4) NOT NULL PRIMARY KEY,
+    roomsize INTEGER NOT NULL
 );
 
 CREATE TABLE player (
-    username VARCHAR(15) PRIMARY KEY
+    username VARCHAR(15) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE hosts (
-    username VARCHAR(15),
-    roomcode VARCHAR(4),
-    FOREIGN KEY (username) REFERENCES player (username),
-    FOREIGN KEY (roomcode) REFERENCES lobby (roomcode)
+    username VARCHAR(15) NOT NULL,
+    roomcode VARCHAR(4) NOT NULL,
+    FOREIGN KEY (username) REFERENCES player (username) ON DELETE CASCADE,
+    FOREIGN KEY (roomcode) REFERENCES lobby (roomcode) ON DELETE CASCADE
 );
 
 CREATE TABLE joins (
-    username VARCHAR(15),
-    roomcode VARCHAR(4),
-    FOREIGN KEY (username) REFERENCES player (username),
-    FOREIGN KEY (roomcode) REFERENCES lobby (roomcode)
+    username VARCHAR(15) NOT NULL,
+    roomcode VARCHAR(4) NOT NULL,
+    FOREIGN KEY (username) REFERENCES player (username) ON DELETE CASCADE,
+    FOREIGN KEY (roomcode) REFERENCES lobby (roomcode) ON DELETE CASCADE
 );
