d3ef008612c6ad5e2e3331ed924ac0754e90ba76
AustinoBlack <baustinblack98@gmail.com>
2023-05-10 13:53:12 -0400

    implemented Leave and End game button logic into the main application

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # Main app
 # @Austin Black
 from flask import Flask, url_for, render_template, redirect, request, jsonify
-from Cogs import Generate_Code, Create_Lobby, Join_Lobby, Get_Players
+from Cogs import Generate_Code, Create_Lobby, Join_Lobby, Get_Players, Leave_Lobby, End_Game
 import json
 
 app = Flask(__name__)
@@ -27,39 +27,49 @@ def Handle_Clicks():
         if request.form.get('return') == 'Leave':                               #if leave button is clicked
             return render_template("Landing.html")                              #go to Landing page
 
-        elif request.form.get('insert') == 'Create':                            #if create button is clicked on create page
-            rmcode = Generate_Code()                                            #see Cogs
-            hname = request.form['hostname']                                    #get hostname from form
-            rmsize = request.form['roomsize']                                   #get roomsize from form
-            if Create_Lobby( hname, rmcode, rmsize ) == True:                   #see Cogs
-                return redirect( url_for('Lobby_Page', room_code=rmcode))       #go to correct lobby
-            elif Create_Lobby( hname, rmcode, rmsize ) == "username":           #refresh page with username taken msg
+        elif request.form.get('end') == 'End Game':
+            rmcode = request.form['roomcode']
+            if End_Game(rmcode) == True:
+                return redirect( url_for('Landing_Page') )
+
+        elif request.form.get('leave') == 'Leave Game':
+            playername = request.form['playername']
+            if Leave_Lobby(playername) == True:
+                return redirect( url_for('Landing_Page') )
+
+        elif request.form.get('insert') == 'Create':                                        #if create button is clicked on create page
+            rmcode = Generate_Code()                                                        #see Cogs
+            hname = request.form['hostname']                                                #get hostname from form
+            rmsize = request.form['roomsize']                                               #get roomsize from form
+            if Create_Lobby( hname, rmcode, rmsize ) == True:                               #see Cogs
+                return redirect( url_for('Lobby_Page', room_code=rmcode, playername=hname)) #go to correct lobby
+            elif Create_Lobby( hname, rmcode, rmsize ) == "username":                       #refresh page with username taken msg
                 return render_template('Create.html', flash_msg="username")
-            elif Create_Lobby( hname, rmcode, rmsize ) == "roomcode":           #refresh page with roomcode already generated msg
+            elif Create_Lobby( hname, rmcode, rmsize ) == "roomcode":                       #refresh page with roomcode already generated msg
                 return render_template('Create.html', flash_msg="roomcode")
             else:
-                return render_template('Create.html', flash_msg="error")        #refresh page with backup\undefined msg
+                return render_template('Create.html', flash_msg="error")                    #refresh page with backup\undefined msg
 
-        elif request.form.get('insert') == 'Join':                              #if join button is clicked on join page
-            rmcode = request.form['roomcode']                                   #get roomcode from form
-            uname = request.form['username']                                    #get username from form
-            if Join_Lobby( uname, rmcode ) == True:                             #see Cogs
-                return redirect( url_for('Lobby_Page', room_code=rmcode))       #go to correct lobby
-            elif Join_Lobby( uname, rmcode ) == "username":                     #refresh page with username taken msg
+        elif request.form.get('insert') == 'Join':                                          #if join button is clicked on join page
+            rmcode = request.form['roomcode']                                               #get roomcode from form
+            uname = request.form['username']                                                #get username from form
+            if Join_Lobby( uname, rmcode ) == True:                                         #see Cogs
+                return redirect( url_for('Lobby_Page', room_code=rmcode, playername=uname ))#go to correct lobby
+            elif Join_Lobby( uname, rmcode ) == "username":                                 #refresh page with username taken msg
                 return render_template('Join.html', flash_msg="username") 
-            elif Join_Lobby( uname, rmcode ) == "room":                         #refresh page with room does not exist msg
+            elif Join_Lobby( uname, rmcode ) == "room":                                     #refresh page with room does not exist msg
                 return render_template('Join.html', flash_msg="room") 
-            elif Join_Lobby( uname, rmcode ) == "full":                         #refresh page with room is full msg
+            elif Join_Lobby( uname, rmcode ) == "full":                                     #refresh page with room is full msg
                 return render_template('Join.html', flash_msg="full") 
             else:
-                return render_template('Join.html', flash_msg="error")          #refresh page with backup\undefined msg
+                return render_template('Join.html', flash_msg="error")                      #refresh page with backup\undefined msg
     else:
-        return render_template("Landing.html")                                  #if something goes horribly wrong, go back to Landing
+        return render_template("Landing.html")                                              #if something goes horribly wrong, go back to Landing
 
 
-@app.route("/Lobby/<room_code>")
-def Lobby_Page(room_code):
-    return render_template("Lobby.html", code=room_code)
+@app.route("/Lobby/<room_code>/<playername>")
+def Lobby_Page(room_code, playername):
+    return render_template("Lobby.html", code=room_code, name=playername)
 
 
 @app.route("/update_list/<room_code>", methods=['GET','POST'])
